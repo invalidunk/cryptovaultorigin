8cec8d9a937588310719de6441ab063adcae6bc3
invalidunk <p.duongtech01258@gmail.com>
2026-04-01 03:32:55 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from '@/components/ui/sonner';
 import Navigation from '@/components/Navigation';
 import './i18n';
